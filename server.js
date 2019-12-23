c0029a750d0ca43e18df6e2475f7108ed10073a3
Manvendra Singh <mjdeora@gmail.com>
2019-12-23 14:01:34 +0530

    Mongodb connection WIP

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors')
 const path = require('path');
-// const util = require('util');
 const multer = require('multer');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const vision = require('@google-cloud/vision');
+const { Storage } = require('@google-cloud/storage');
+// const { Datastore } = require('@google-cloud/datastore');
+const { ImageAnnotatorClient } = require('@google-cloud/vision');
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-require('dotenv').config();
-
-// Creates a client
-// process.env.SERVICE_ACCOUNT_JSON
-const visionClient = new vision.ImageAnnotatorClient();
-
-const port = process.env.PORT || 9000;
 
 const app = express();
 
@@ -24,6 +20,10 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+require('dotenv').config();
+
+const port = process.env.PORT || 9000;
 
 const uploadPath = path.join(__dirname, 'uploads');
 
@@ -35,7 +35,57 @@ if (!fs.existsSync(uploadPath)) {
 // configure multer to use the uploads folder
 const upload = multer({ dest: 'uploads/' });
 
+
+
+
+const storage = new Storage();
+
+const bucketName = 'handwriting-recognition-221';
+const bucket = storage.bucket(bucketName);
+
+
+
+// const datastore = new Datastore();
+
+
+
+const visionClient = new ImageAnnotatorClient();
+
+
+
+
+const Schema = mongoose.Schema;
+
+let ImageSchema = new Schema({
+    _id: { type: String, required: true },
+    name: { type: String, required: true, max: 100 },
+});
+
+let ImageModal = mongoose.model('Image', ImageSchema);
+
+const uri = `mongodb+srv://canvas:<${process.env.DB_PASSWORD}>@cluster0-ovurt.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(uri);
+
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// let collection = null
+
+// client.connect(err => {
+//     collection = client.db("handwriting-recognition").collection("data");
+//     // console.log("collection ", collection)
+//     // client.close();
+// });
+
+
+
+
+
 // API calls
+
 app.get('/api/contents', (req, res) => {
     res.json({ express: 'Hello From Express' });
 });
@@ -53,8 +103,26 @@ app.post('/api/contents', upload.single('document'), async (req, res) => {
         .then(response => {
             const detections = response[0].textAnnotations;
             const description = detections[0].description;
-            console.log('Text:', description);
-            res.status(200).json({ description });
+
+            bucket.upload(filePath, function (err, file, apiResponse) {
+                if (err) {
+                    throw new Error(err)
+                }
+                // console.log("apiResponse ", apiResponse)
+
+                let image = new ImageModal(
+                    {
+                        _id: apiResponse.id,
+                        mediaLink: apiResponse.mediaLink
+                    }
+                );
+
+                image.save(function (err) {
+                    if (err) {
+                        return next(err);
+                    }
+                })
+            })
         })
         .catch(err => {
             console.error(err);
@@ -84,5 +152,3 @@ app.use(function (err, req, res, next) {
 app.listen(port, function () {
     console.log(`Example app listening on port: ${port}`);
 });
-
-module.exports = app;
