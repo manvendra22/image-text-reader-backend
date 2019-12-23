dea72d99d272ad50ba16f213e6d0288b8f6cf4ad
Manvendra Singh <mjdeora@gmail.com>
2019-12-23 10:07:08 +0530

    Vision api bind successfull

--- server.js
+++ server.js
@@ -5,29 +5,22 @@ const path = require('path');
 const multer = require('multer');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-// const fileUpload = require('express-fileupload');
-const { ImageAnnotatorClient } = require('@google-cloud/vision');
+const vision = require('@google-cloud/vision');
 
 require('dotenv').config();
+
+// Creates a client
+// process.env.SERVICE_ACCOUNT_JSON
+const visionClient = new vision.ImageAnnotatorClient();
 
 const port = process.env.PORT || 9000;
 
 const app = express();
 
-// app.use(fileUpload());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// promisify the filesystem functions we need
-// const readdirAsync = util.promisify(fs.readdir);
-// const statAsync = util.promisify(fs.stat);
-// const unlinkAsync = util.promisify(fs.unlink);
-
-// fs.writeFileSync(path.join(__dirname, 'gcloud-credentials.json'), process.env.SERVICE_ACCOUNT_JSON);
-
-// create Cloud Vision client
-// const visionClient = new ImageAnnotatorClient();
 
 const uploadPath = path.join(__dirname, 'uploads');
 
@@ -44,22 +37,25 @@ app.get('/api/contents', (req, res) => {
     res.json({ express: 'Hello From Express' });
 });
 
-app.post('/api/contents', upload.single('image'), async (req, res) => {
-    if (!req.file) {
-        res.sendStatus(500);
-        return;
+app.post('/api/contents', upload.single('document'), async (req, res) => {
+    try {
+        if (!req.file) {
+            throw new Error('File not found')
+        }
+
+        // get the file path uploaded via multer
+        const filePath = req.file.path;
+
+        const [result] = await visionClient.textDetection(filePath);
+        const detections = result.textAnnotations;
+        const description = detections.description;
+        console.log('Text:', description);
+        res.json({
+            description
+        })
+    } catch (err) {
+        throw new Error(err)
     }
-
-    // get the file path uploaded via multer
-    const filePath = req.file.path;
-
-    console.log("filePath ", filePath)
-
-    // send the image to gcloud for text detection
-    // const results = await visionClient.textDetection(filePath);
-
-    // console.log("results ", results)
-    // res.status(201).json(results);
 })
 
 // catch 404 and forward to error handler
@@ -71,13 +67,14 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+    console.log(err)
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.json({ error: err })
 });
 
 app.listen(port, function () {
