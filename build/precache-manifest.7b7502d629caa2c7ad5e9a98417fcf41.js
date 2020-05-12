d79e2e93f05628b3c4a6f32328f9658f0f14e9fe
Manvendra Singh <mjdeora@gmail.com>
2020-05-12 15:22:38 +0530

    [Fix] Hack

---
+++ build/precache-manifest.7b7502d629caa2c7ad5e9a98417fcf41.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462160b1bd1979d0a40b1e560d5168e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10a0c464673c6a30bc57",
+    "url": "/static/css/main.6b4d4388.chunk.css"
+  },
+  {
+    "revision": "1b8a5f25ef04893126b6",
+    "url": "/static/js/2.ad4bde39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad4bde39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a0c464673c6a30bc57",
+    "url": "/static/js/main.b36d6ddd.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "f16429a9e53ae7c5df8fb48b3a98d8a2",
+    "url": "/static/media/failed.f16429a9.svg"
+  },
+  {
+    "revision": "860a4c627c9c7d0cb307229b5febf90d",
+    "url": "/static/media/github.860a4c62.svg"
+  },
+  {
+    "revision": "2b0f570ecddf8cbf802d299776b5359d",
+    "url": "/static/media/photo.2b0f570e.svg"
+  },
+  {
+    "revision": "82ff87d3a4cc00292d8d63d2e1677d0b",
+    "url": "/static/media/upload.82ff87d3.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /**
 Serve static files from the React app
 */
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 require('dotenv').config();
@@ -159,7 +159,7 @@ The "catchall" handler: for any request that doesn't
 match one above, send back React's index.html file.
 */
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'));
+    res.sendFile(path.join(__dirname + 'build/index.html'));
 });
 
 /**
